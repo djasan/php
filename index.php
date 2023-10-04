fb8c4eacc00812300f1ab263c6322c7077cfc643
djamila key <sansondjamila@orange.fr>
2023-10-04 12:27:16 +0200

    c1

--- index.php
+++ index.php
@@ -2,12 +2,55 @@
 
     <?php
 
-    include("./header.php");
-    echo "<h1>encore ceppic</h1>"; ?>
+    include './header.php';
+    require './footer.php';
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium cumque, nisi commodi ipsum molestias inventore animi sunt tempora blanditiis necessitatibus, odio autem natus ipsam iusto incidunt illum repellat, corporis soluta.</p>
+    echo "<h1>encore ceppic</h1>";
 
-    <?php
-    echo "<h1>encore ceppic</h1>"; ?>
+    // $_variable = 'une variable';
+
+    /*   $variable = 'une variable';
+
+
+    require './footer.php'; */
+
+    /*  echo $_variable . $_Variable;  */
+    // echo $_variable . " " . $_Variable;
+
+    /*  echo '$variable .$variable'; */
+
+    /*  $camel_case = "une variable en camel case";
+ $kebab_case = "une variable en kebab case";
+ $snake_case = "une variable en snake case";
+ $PascalCase= "une variable en pascal case"; */
+
+    /* $nombre = "45";
+    $nombreDecimal = "10.5";
+    $boolean = "true or false";
+    var_dump($nombre);
+    print_r($variable); */
+
+  /*   $fruit1 = "pomme";
+    $fruit2 = "banane";
+
+    var_dump($fruit1);
+
+
+
+    echo "<p> $fruit1" . "<br>" . "$fruit2</p>"; */
+/* 
+echo "Aujourd'\hui il fait beau"; */
+
+$resultat ="";
+$resultat ="<h1>";
+$resultat ="cours php";
+$resultat ="<h1>";
+echo $resultat;
+
+
+
+    require './footer.php';
+
+    ?>
 
 
