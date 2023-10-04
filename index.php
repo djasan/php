f7ab8e69afa7b6147fe4dd92b9ef7e0d012f4602
djamila key <sansondjamila@orange.fr>
2023-10-04 10:32:03 +0200

    php

---
+++ index.php
@@ -0,0 +1,13 @@
+
+
+    <?php
+
+    include("./header.php");
+    echo "<h1>encore ceppic</h1>"; ?>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium cumque, nisi commodi ipsum molestias inventore animi sunt tempora blanditiis necessitatibus, odio autem natus ipsam iusto incidunt illum repellat, corporis soluta.</p>
+
+    <?php
+    echo "<h1>encore ceppic</h1>"; ?>
+
+
