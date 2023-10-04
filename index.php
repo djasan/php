edc3dcc0a5a605474173562edfd9856f994b4dbc
djamila key <sansondjamila@orange.fr>
2023-10-04 16:38:24 +0200

    sv

--- index.php
+++ index.php
@@ -1,9 +1,57 @@
 <?php
-
+/* phpinfo(); */
 // include './header.php';
 // include_once './header.php';
 require './fonction.php';
 $titre ="Operateurs.php";
+
+/* $x=4;
+$y=2;
+$z=5; */
+/* + - /  *  ** % */
+
+/* $calcul=($x+$y);
+dd($calcul); */
+/* $calcul=($x*$y);
+dd($calcul); */
+/* $calcul=($x/$y);
+dd($calcul); */
+/* $calcul=($x**$y);
+dd($calcul); */
+/* $calcul=($x%$y);
+dd($calcul); */
+
+/* $calcul=($x+$y)*$z;
+dd($calcul); */
+/* $calcul=($x+$y*$z);
+dd($calcul); */
+
+/* $calcul=($x+=5);
+dd($calcul);
+ */
+/* $calcul=($x-=5);
+dd($calcul); */
+/* $calcul=($x%=5);
+dd($calcul); */
+
+
+/* $anneeDeNaissance =1970;
+
+$age=date("2023")-
+$anneeDeNaissance;
+
+
+dd($age); */
+
+/* calcul prix ttc */
+/* $prixHT=34;
+
+$tva=10;
+$prixTTC=$prixHT +($tva*$prixHT)/100;
+dd($prixTTC); */
+
+
+
 
 require "./index.view.php";
 
