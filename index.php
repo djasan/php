2e82121fada4f2f93b17885845d69864c4a154bc
djamila key <sansondjamila@orange.fr>
2023-10-06 16:55:18 +0200

    sv

--- index.php
+++ index.php
@@ -1,29 +1,39 @@
 <?php
-require "./fonction.php";
+/* require "./fonction.php"; */
 
-$titre = "Boucles php";
+$titre = "structure de contrôle bis";
+
+
+/* $age = 20;
+if ($age >= 18) :
+    echo 'peut voter.';else : 
+    echo 'ne peut pas voter!!!';
+endif; */
 /* 
-$planetes = ['mars', 'terre', 'uranus', 'venus', 'jupiter', 'mercure'];
-$planetes2 = [
-    'm' => 'mars',
-    't' => 'terre',
-    'u' => 'uranus',
-    'v' => 'venus',
-    'j' => 'jupiter',
-]; */
-
-/* dbug($planetes2); */
-$user =[
-    'nom' => 'Doucet Lucien',
-    'email' => 'ldoupet@yaya.com',
-    'competence'=> ['php','react','python']
-];
-
-  foreach($user as $ket => $value):
-    echo $key . '.' . $value;
- endforeach;
-
-dbug($user);
+$heure =15;
+if($heure>=12):
+echo'on est le matin'; 
+elseif($heure<=18):
+    echo'on est le soir'; 
+else : 
 
 
- require './index.view.php'; 
+    echo'on est l apres-midi.';
+endif;
+ */
+
+
+
+ $a = 20;
+ $b = "5";
+ 
+ if ($b == 3):
+     echo "la variable \$b est égale à 3";
+ endif;
+ ?>
+ 
+
+
+
+
+<!--  require './index.view.php';  -->
