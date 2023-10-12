5ec4763d681aaf5257cad3795486d86d85c5f0a5
djamila key <sansondjamila@orange.fr>
2023-10-12 16:58:34 +0200

    sv

--- index.php
+++ index.php
@@ -1,46 +1,33 @@
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 
 
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
 
 
-    <title>Formulaire</title>
-</head>
 
-<body>
-    <h1>Formulaire</h1>
-    <form action="./traitement.php" method="POST">
-        <label for="nom">Nom :
-            <input type="text" name="nom" placeholder="Votre nom">
-        </label><br>
-        <label for="email">Email :
-            <input type="email" name="email" placeholder="Votre email">
-        </label><br>
-        <label for="commentaire"> Commentaire :</label>
-        <textarea name="commentaire" id="commentaire" cols="30" rows="10"></textarea>
-        <label for="niveau">
-            Niveau :
-            <input type="radio" name="niveau" value="debutant">Débutant
-            <input type="radio" name="niveau" value="Intermediaire">Intermediaire
-            <input type="radio" name="niveau" value="Expert">Expert
-        </label><br>
-        <label for="competences">
-            Compétences :
-            PHP<input type="checkbox" name="competences[]" value="php">
-            Python<input type="checkbox" name="competences[]" value="python">
-            Css<input type="checkbox" name="competences[]" value="css">
-            Javascript<input type="checkbox" name="competences[]" value="javascript">
-        </label><br>
-        <input type="submit" name="submitted" value="Valider">
-    </form>
-</body>
+<?php
+require './fonction.php';
+// dd($_SERVER);
+// dd($_SERVER["REQUEST_URI"] );
 
-</html>
+$url = 'http://http://localhost/php/contact.php';
+
+dd(parse_url ($_SERVER["REQUEST_URI"]))["path"];
+if($url==="php");
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
