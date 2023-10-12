74236917c9cf35336e11733592b6b4864cc64fc2
djamila key <sansondjamila@orange.fr>
2023-10-12 15:57:27 +0200

    sv

--- index.php
+++ index.php
@@ -1,51 +1,23 @@
-<?php
-require './fonction.php';
 
-// dbug($_GET);
-
-if(isset($_POST["submitted"])):
-// dbug($_POST);
-echo "Votre nom est : ", $_POST['nom'], '<br>';
-echo "Votre email est : ", $_POST['email'], '<br>';
-echo "Votre commentaire est : ", $_POST['commentaire'], '<br>';
-echo "Votre niveau est : ", $_POST['niveau'], '<br>';
-$competences = $_POST['competences'];
-
-foreach ($competences as $competence) {
-    if ($competence === 'css') {
-        echo "Vous avez la compétence CSS", "<br>";
-    }
-}
-foreach ($competences as $competence) {
-    if ($competence === 'javascript') {
-        echo "Vous avez la compétence JavaScript", "<br>";
-    }
-}
-foreach ($competences as $competence) {
-    if ($competence === 'php') {
-        echo "Vous avez la compétence PHP", "<br>";
-    }
-}
-foreach ($competences as $competence) {
-    if ($competence === 'python') {
-        echo "Vous avez la compétence Python", "<br>";
-    }
-}
-endif;
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    
+
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+
+
     <title>Formulaire</title>
 </head>
 
 <body>
     <h1>Formulaire</h1>
-    <form action="" method="POST">
+    <form action="./traitement.php" method="POST">
         <label for="nom">Nom :
             <input type="text" name="nom" placeholder="Votre nom">
         </label><br>
